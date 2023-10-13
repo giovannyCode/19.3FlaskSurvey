758da90f699ccd3d7837be4d9c5be1fd87fb7d83
Giovanny Bejarano <giovanny10@msn.com>
2023-10-13 18:26:03 -0400

    add Session variable

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect,flash
+from flask import Flask, request, render_template, redirect,flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] ="oh-so-secret"
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-responses = []
+
 
 @app.route("/")
 def home_page():
@@ -15,8 +15,23 @@ def home_page():
   survey_instructions =satisfaction_survey.instructions
   return render_template("homepage.html", survey_tittle=survey_tittle,survey_instructions=survey_instructions)
 
+
+@app.route("/session_setter",methods=["POST"] )
+def session_setter():
+ survey_tittle =satisfaction_survey.title
+ survey_instructions =satisfaction_survey.instructions
+ session["responses"] = []
+ print(f"session data for responses {session['responses']}")
+ return redirect("/questions/0")
+
+
+
 @app.route("/questions/<int:questionNum>")
 def questions(questionNum):
+  responses = session["responses"]
+  print("In questions")
+  print(f"number_of_responses:{len(responses)} ")
+  print(f"responses:{responses} ")
   number_of_responses = len(responses)
   if number_of_responses == questionNum:
     survey_tittle =satisfaction_survey.title
@@ -37,10 +52,14 @@ def questions(questionNum):
 
 @app.route("/answer",methods=["POST"])
 def answer():
+  responses =session["responses"]
   response = request.form["response"]
   responses.append(response)
+  session["responses"] = responses
   current_question =int(request.form["questionNum"])
   next_question =current_question +1
+  print(f"current_question:  {current_question } next question:{next_question}" )
+  print("Printing responses from session")
   print(responses)
   return redirect(f"/questions/{next_question}")
 
